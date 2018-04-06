9d81e07c0fe975ad22e0cf92851034c32dc010b1
Pablo Barreyro <Pabarreyro@gmail.com>
2018-04-06 10:38:02 -0700

    add tested and refactored constructors for Order, Pizza, and Topping

---
+++ js/scripts.js
@@ -0,0 +1,18 @@
+//Backend
+function Pizza(size){
+  this.size = size;
+  this.toppings = [];
+  this.price = this.size * 5;
+}
+
+function Topping(name, price){
+  this.name = name;
+  this.price = price;
+}
+
+function Order(){
+  this.pizzas = [];
+  this.subTotal = 0;
+}
+
+//Frontend
