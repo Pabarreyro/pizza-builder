c1969fa87ed0dfc64f89ec92cd7136c2c06e7513
Pablo Barreyro <Pabarreyro@gmail.com>
2018-04-06 11:33:35 -0700

    add tests for entier backend sequence

--- js/scripts.js
+++ js/scripts.js
@@ -33,4 +33,29 @@ Order.prototype.priceOrder = function () {
   }, 0);
 }
 
+// Test
+let Anchovy = new Topping("Anchovy", 2);
+let Cheese = new Topping("Cheese", 0);
+let Pepperoni = new Topping("Pepperoni", 1);
+let Mushrooms = new Topping("Mushrooms", 1);
+let Pineapple = new Topping("Pineapple", 1);
+
+let newOrder = new Order();
+console.log(newOrder);
+
+let newPizza = new Pizza(2);
+console.log(newPizza);
+newPizza.addTopping(Cheese);
+newPizza.pricePizza();
+console.log(newPizza);
+newPizza.addTopping(Anchovy);
+newPizza.pricePizza();
+console.log(newPizza);
+newPizza.addTopping(Mushrooms);
+newPizza.pricePizza();
+console.log(newPizza);
+newOrder.addPizza(newPizza);
+newOrder.priceOrder();
+console.log(newOrder);
+
 //Frontend
